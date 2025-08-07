ce78de24a40c46200d915905f27845322fc51835
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-06 22:37:25 -0400

    Elimina la ruta del componente Formulario en App.jsx, deshabilitando su acceso temporalmente.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/formulario" element={<Formulario />} />
-        {/*<Route path="/ver" element={<Storage />} />*/}
       </Routes>
     </Router>
   );
